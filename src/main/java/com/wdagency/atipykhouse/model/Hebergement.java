abd9d1bdf31b3fd704cc647e7e7d37766e8b7879
stefOuroboros <stephane.gallois89@gmail.com>
2020-12-01 11:37:43 +0100

    fix type entity Blob

--- src/main/java/com/wdagency/atipykhouse/model/Hebergement.java
+++ src/main/java/com/wdagency/atipykhouse/model/Hebergement.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -55,7 +56,8 @@ public class Hebergement {
     private Double latitude;
     
 	@Column(name="photos")
-	private List<Blob> photos; 
+	@Lob
+	private List<Blob> photos;
     
 	@ManyToOne(targetEntity = Type.class, cascade = CascadeType.MERGE)
 	@JoinColumn(name = "typeName", nullable=false)
